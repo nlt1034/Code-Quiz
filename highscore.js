6479d0c72abb59f86bca99d2aa737427c97c922d
Nate Taylor <nlt1034@uncw.edu>
2021-03-01 12:57:03 -0500

    Added all html, css, and js files to github

---
+++ highscore.js
@@ -0,0 +1,30 @@
+function printHighscores() {
+    // retrieve scores from local storage
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+    // sort highscores (descending)
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // create li tag
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+  
+      // display
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+  
+  document.getElementById("clear").onclick = clearHighscores;
+  
+  // run function
+  printHighscores();
+  
